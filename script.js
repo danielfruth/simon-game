bab310818bb419daf11dd0446c13abca714f94a5
danielfruth <moto_386@outlook.com>
2020-01-06 12:10:28 -0700

    fixed iteration problem with forEach() method

--- script.js
+++ script.js
@@ -20,34 +20,38 @@ function lightButtons() {
   compArray.forEach(lightMe);
 }
 
-function lightMe(buttonNum) {
-  if (buttonNum === 0) {
-    setTimeout(function() {
-      greenButton.style.background = '#75ff79';
-    }, 500);
-    setTimeout(function() {
-      greenButton.style.background = 'green';
-    }, 1000);
-  } else if (buttonNum === 1) {
-    setTimeout(function() {
-      redButton.style.background = '#ff7a83';
-    }, 500);
-    setTimeout(function() {
-      redButton.style.background = 'red';
-    }, 1000);
-  } else if (buttonNum === 2) {
-    setTimeout(function() {
-      yellowButton.style.background = '#ffffb5';
-    }, 500);
-    setTimeout(function() {
-      yellowButton.style.background = 'yellow';
-    }, 1000);
-  } else {
-    setTimeout(function() {
-      blueButton.style.background = '#7c83ff';
-    }, 500);
-    setTimeout(function() {
-      blueButton.style.background = 'blue';
-    }, 1000);
-  }
+// Sage Kearney helped with the iterarion pause on my forEach method by
+// linking me to https://travishorn.com/delaying-foreach-iterations-2ebd4b29ad30
+function lightMe(buttonNum, i) {
+  setTimeout(() => {
+    if (buttonNum === 0) {
+      setTimeout(function() {
+        greenButton.style.background = '#75ff79';
+      }, 500);
+      setTimeout(function() {
+        greenButton.style.background = 'green';
+      }, 1000);
+    } else if (buttonNum === 1) {
+      setTimeout(function() {
+        redButton.style.background = '#ff7a83';
+      }, 500);
+      setTimeout(function() {
+        redButton.style.background = 'red';
+      }, 1000);
+    } else if (buttonNum === 2) {
+      setTimeout(function() {
+        yellowButton.style.background = '#ffffb5';
+      }, 500);
+      setTimeout(function() {
+        yellowButton.style.background = 'yellow';
+      }, 1000);
+    } else {
+      setTimeout(function() {
+        blueButton.style.background = '#7c83ff';
+      }, 500);
+      setTimeout(function() {
+        blueButton.style.background = 'blue';
+      }, 1000);
+    }
+  }, i * 1000);
 }
